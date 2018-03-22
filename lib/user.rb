d3b8a6d368cbfada3eacc100a3e64fa32d1b661b
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-22 19:34:46 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,7 +2,7 @@ class User
   
   attr_accessor :first_name, :last_name
   
-    def initialize(first_name, last_name)
+    def initializeze(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
   end
